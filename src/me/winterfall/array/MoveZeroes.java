0467e1114b4b3477687e43c8a50755e24ef63d3f
winterfellding <winterfall.ding@gmail.com>
2018-07-08 00:32:34 +0800

    move zeroes

---
+++ src/me/winterfall/array/MoveZeroes.java
@@ -0,0 +1,23 @@
+package me.winterfall.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        List<Integer> noneZeroIdx = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                noneZeroIdx.add(i);
+            }
+        }
+
+        int aryIdx = 0;
+        for (int idx : noneZeroIdx) {
+            nums[aryIdx++] = nums[idx];
+        }
+        for (int i = aryIdx; i < nums.length; i++) {
+            nums[i] =  0;
+        }
+    }
+}
